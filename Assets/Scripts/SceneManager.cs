7386aaa842805f0fd2e966c53456a56719e60e5d
Smudgy0 <theohammond2006@gmail.com>
2024-12-03 13:08:51 +0000

    Initial commit

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager
+{
+    public void Example1Load()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
